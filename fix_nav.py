08c42604975061524d15076a17d82099e8df7dfc
trvlas96 <trvlasova96@gmail.com>
2026-09-15 15:17:45 +0500

    Update logo: remove background completely

--- fix_nav.py
+++
@@ -1,8 +0,0 @@
-with open('index.html', 'r', encoding='utf-8') as f:
-    html = f.read()
-
-html = html.replace('href="#services_price"', 'href="#services"')
-
-with open('index.html', 'w', encoding='utf-8') as f:
-    f.write(html)
-print("Nav fixed")
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/beauty-sekta/',
+  base: '/agma/',
   server: {
     host: '127.0.0.1',
     port: 5173
